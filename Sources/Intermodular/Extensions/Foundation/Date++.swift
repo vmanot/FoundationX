e5e370ed39a3c056e14ee92f215b4dd72ea6c2e5
Vatsal Manot <vatsal.manot@yahoo.com>
2020-08-25 20:34:11 +0530

    Update package

--- Sources/Intermodular/Extensions/Foundation/Date++.swift
+++ Sources/Intermodular/Extensions/Foundation/Date++.swift
@@ -12,9 +12,21 @@ extension Date {
             .then({ $0.dateFormat = format })
             .string(from: self)
     }
-}
-
-extension Date {
+    
+    public func get(
+        _ components: Calendar.Component...,
+        calendar: Calendar = Calendar.current
+    ) -> DateComponents {
+        return calendar.dateComponents(Set(components), from: self)
+    }
+    
+    public func get(
+        _ component: Calendar.Component,
+        calendar: Calendar = Calendar.current
+    ) -> Int {
+        return calendar.component(component, from: self)
+    }
+    
     /// Number of days (relative to this date) till a given date.
     public func days(till other: Date, in calendar: Calendar = .current) -> Int! {
         calendar.dateComponents([.day], from: self, to: other).day
@@ -51,7 +63,7 @@ extension Date {
     }
     
     public var startOfMonth: Date {
-        return Calendar.current.date(from: Calendar.current.dateComponents([.year, .month], from: startOfDay))!
+        Calendar.current.date(from: startOfDay.get(.year, .month))!
     }
     
     public var endOfMonth: Date {
@@ -65,31 +77,31 @@ extension Date {
 }
 
 extension Date {
-    public var yearsFromNow: Int {
+    public var yearsToNow: Int {
         Calendar.current.dateComponents([.year], from: self, to: Date()).year ?? 0
     }
     
-    public var monthsFromNow: Int {
+    public var monthsToNow: Int {
         Calendar.current.dateComponents([.month], from: self, to: Date()).month ?? 0
     }
     
-    public var weeksFromNow: Int {
+    public var weeksToNow: Int {
         Calendar.current.dateComponents([.weekOfYear], from: self, to: Date()).weekOfYear ?? 0
     }
     
-    public var daysFromNow: Int {
+    public var daysToNow: Int {
         Calendar.current.dateComponents([.day], from: self, to: Date()).day ?? 0
     }
     
-    public var hoursFromNow: Int {
+    public var hoursToNow: Int {
         Calendar.current.dateComponents([.hour], from: self, to: Date()).hour ?? 0
     }
     
-    public var minutesFromNow: Int {
+    public var minutesToNow: Int {
         Calendar.current.dateComponents([.minute], from: self, to: Date()).minute ?? 0
     }
     
-    public var secondsFromNow: Int {
+    public var secondsToNow: Int {
         Calendar.current.dateComponents([.second], from: self, to: Date()).second ?? 0
     }
     
@@ -97,35 +109,35 @@ extension Date {
         return Calendar.current.dateComponents([.second], from: other, to: self).second ?? 0
     }
     
-    public var relativeTime: String? {
-        if yearsFromNow > 0 {
-            return "\(yearsFromNow) year" + (yearsFromNow    > 1 ? "s" : "") + " ago"
+    public var relativeTimeDescription: String? {
+        if yearsToNow > 0 {
+            return "\(yearsToNow) year" + (yearsToNow > 1 ? "s" : "") + " ago"
         }
         
-        if monthsFromNow > 0 {
-            return "\(monthsFromNow) month" + (monthsFromNow   > 1 ? "s" : "") + " ago"
+        if monthsToNow > 0 {
+            return "\(monthsToNow) month" + (monthsToNow > 1 ? "s" : "") + " ago"
         }
         
-        if weeksFromNow > 0 {
-            return "\(weeksFromNow) week" + (weeksFromNow    > 1 ? "s" : "") + " ago"
+        if weeksToNow > 0 {
+            return "\(weeksToNow) week" + (weeksToNow > 1 ? "s" : "") + " ago"
         }
         
-        if daysFromNow > 0 {
-            return daysFromNow == 1 ? "Yesterday" : "\(daysFromNow) days ago"
+        if daysToNow > 0 {
+            return daysToNow == 1 ? "Yesterday" : "\(daysToNow) days ago"
         }
         
-        if hoursFromNow > 0 {
-            return "\(hoursFromNow) hour" + (hoursFromNow > 1 ? "s" : "") + " ago"
+        if hoursToNow > 0 {
+            return "\(hoursToNow) hour" + (hoursToNow > 1 ? "s" : "") + " ago"
         }
         
-        if minutesFromNow > 0 {
-            return "\(minutesFromNow) minute" + (minutesFromNow > 1 ? "s" : "") + " ago"
+        if minutesToNow > 0 {
+            return "\(minutesToNow) minute" + (minutesToNow > 1 ? "s" : "") + " ago"
         }
         
-        if secondsFromNow > 0 {
-            return secondsFromNow < 15
+        if secondsToNow > 0 {
+            return secondsToNow < 15
                 ? "Just now"
-                : "\(secondsFromNow) second" + (secondsFromNow > 1 ? "s" : "") + " ago"
+                : "\(secondsToNow) second" + (secondsToNow > 1 ? "s" : "") + " ago"
         }
         
         return nil
