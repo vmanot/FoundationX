a7f582625cc68e102ab3d8804906b3a31a615437
Vatsal Manot <vatsal.manot@yahoo.com>
2020-12-08 22:02:31 -0500

    Update package

--- Sources/Intramodular/Caching/Cache.swift
+++ Sources/Intramodular/Caching/Cache.swift
@@ -16,28 +16,6 @@ public protocol CacheProtocol {
     func removeAllCachedValues() throws
 }
 
-public protocol Caching {
-    associatedtype Cache: CacheProtocol
-    
-    var cache: Cache { get }
-}
-
-private var Caching_cache_objcAssociationKey: Void = ()
-
-extension Caching where Self: AnyObject, Cache: Initiable {
-    public var cache: Cache {
-        if let result = objc_getAssociatedObject(self, &Caching_cache_objcAssociationKey) as? Cache {
-            return result
-        } else {
-            let result = Cache()
-            
-            objc_setAssociatedObject(self, &Caching_cache_objcAssociationKey, result, .OBJC_ASSOCIATION_RETAIN)
-            
-            return result
-        }
-    }
-}
-
 // MARK: - Conformances -
 
 public protocol _NoCacheType: Initiable & CacheProtocol {
