ab75eb6bab0fda1d8a474c38a6de5a0e36197c13
Vatsal Manot <vatsal.manot@yahoo.com>
2021-02-01 13:15:34 +0530

    Update package

--- Sources/Intramodular/RegEx/RegularExpression+Capture.swift
+++ Sources/Intramodular/RegEx/RegularExpression+Capture.swift
@@ -6,6 +6,79 @@ import Foundation
 import Swallow
 
 extension RegularExpression {
+    public func capture(_ expression: RegularExpression, named name: String? = nil) -> RegularExpression {
+        self + expression.captureGroup(named: name)
+    }
+    
+    public func capture(_ name: String? = nil, _ closure: ((RegularExpression) -> RegularExpression)) -> RegularExpression {
+        capture(closure(.init()), named: name)
+    }
+    
+    public func capture(_ option: Target, named name: String? = nil) -> RegularExpression {
+        capture(RegEx.match(option), named: name)
+    }
+    
+    public func capture(oneOf strings: String...) -> RegularExpression {
+        capture(.oneOf(strings))
+    }
+    
+    public func capture(anyOf string: String) -> RegularExpression {
+        capture(RegEx.match(anyOf: .string(string)))
+    }
+    
+    public func capture(anyOf string: String, repeating count: Int) -> RegularExpression {
+        capture(RegEx.match(anyOf: .string(string)).repeating(count))
+    }
+    
+    public func capture(anyOf characterSet: CharacterSet) -> RegularExpression {
+        capture(RegEx.match(anyOf: .characterSet(characterSet)))
+    }
+    
+    public func capture(anyOf characterSet: CharacterSet, repeating count: Int) -> RegularExpression {
+        capture(RegEx.match(anyOf: .characterSet(characterSet)).repeating(count))
+    }
+}
+
+extension RegularExpression {
+    var isContainedInNotCaptureGroup: Bool {
+        (pattern[try: 0..<3] == "(?:") && (pattern.last == Character(")"))
+    }
+    
+    func nonCaptureGroup() -> RegularExpression {
+        guard pattern != "", isValid, !isContainedInNotCaptureGroup else {
+            return self
+        }
+        
+        return .init(pattern: "(?:" + pattern + ")")
+    }
+    
+    func captureGroup(named name: String?) -> RegularExpression {
+        if let name = name {
+            return decomposeNonCaptureGroupIfNecessary().modifyPattern {
+                "(".appending("?<\(name)>").appending($0).appending(")")
+            }
+        } else {
+            return decomposeNonCaptureGroupIfNecessary().modifyPattern {
+                "(".appending($0).appending(")")
+            }
+        }
+    }
+    
+    private func decomposeNonCaptureGroupIfNecessary() -> RegularExpression {
+        guard isContainedInNotCaptureGroup else {
+            return self
+        }
+        
+        var pattern = self.pattern
+        
+        pattern.remove(at: pattern.lastIndex)
+        pattern.removeFirst(3)
+        
+        return .init(pattern: pattern)
+    }
+}
+
+extension RegularExpression {
     public func add(mode: RegularExpression.Options) -> RegularExpression {
         modifyPattern({ "(?\(mode))" + $0 })
     }
@@ -39,72 +112,5 @@ extension RegularExpression {
     
     public func repeating() -> RegularExpression {
         nonCaptureGroup().modifyPattern({ $0.appending("+") })
-    }
-}
-
-extension RegularExpression {
-    public func capture(_ expression: RegularExpression) -> RegularExpression {
-        modifyPattern {
-            $0 + expression.modifyPattern({ "(" + $0 + ")" }).pattern
-        }
-    }
-    
-    public func capture(_ closure: ((RegularExpression) -> RegularExpression)) -> RegularExpression {
-        capture(closure(.init()))
-    }
-    
-    public func capture(_ option: Target) -> RegularExpression {
-        capture(RegEx.match(option))
-    }
-    
-    public func capture(oneOf strings: String...) -> RegularExpression {
-        return capture(.oneOf(strings))
-    }
-    
-    public func capture(anyOf string: String) -> RegularExpression {
-        capture(RegEx.match(anyOf: .string(string)))
-    }
-    
-    public func capture(anyOf string: String, repeating count: Int) -> RegularExpression {
-        capture(RegEx.match(anyOf: .string(string)).repeating(count))
-    }
-    
-    public func capture(anyOf characterSet: CharacterSet) -> RegularExpression {
-        capture(RegEx.match(anyOf: .characterSet(characterSet)))
-    }
-    
-    public func capture(anyOf characterSet: CharacterSet, repeating count: Int) -> RegularExpression {
-        capture(RegEx.match(anyOf: .characterSet(characterSet)).repeating(count))
-    }
-}
-
-extension RegularExpression {
-    var isContainedInNotCaptureGroup: Bool {
-        (pattern[try: 0..<3] == "(?:") && (pattern.last == Character(")"))
-    }
-    
-    func nonCaptureGroup() -> RegularExpression {
-        guard pattern != "", isValid, !isContainedInNotCaptureGroup else {
-            return self
-        }
-        
-        return .init(pattern: "(?:" + pattern + ")")
-    }
-    
-    func captureGroup() -> RegularExpression {
-        decomposeNonCaptureGroupIfNecessary().modifyPattern({ "(".appending($0).appending(")" )})
-    }
-    
-    private func decomposeNonCaptureGroupIfNecessary() -> RegularExpression {
-        guard isContainedInNotCaptureGroup else {
-            return self
-        }
-        
-        var pattern = self.pattern
-        
-        pattern.removeFirst(3)
-        pattern.remove(at: pattern.lastIndex)
-        
-        return .init(pattern: pattern)
     }
 }
