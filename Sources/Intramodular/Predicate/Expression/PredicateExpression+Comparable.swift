a820e743b75915d7a3bb89eb242c19ed6c97568c
Vatsal Manot <vatsal.manot@yahoo.com>
2023-01-10 16:21:46 +0530

    Update package

--- Sources/Intramodular/Predicate/Expression/PredicateExpression+Comparable.swift
+++ Sources/Intramodular/Predicate/Expression/PredicateExpression+Comparable.swift
@@ -17,9 +17,12 @@ public func == <E: PredicateExpression, T: Equatable & PredicateExpressionPrimit
     .comparison(.init(lhs, .equal, rhs))
 }
 
-
 public func == <E: PredicateExpression, T: PredicateExpressionPrimitiveConvertible> (lhs: E, rhs: T) -> Predicate<E.Root> {
     .comparison(.init(lhs, .equal, rhs.toPredicateExpressionPrimitive()))
+}
+
+public func == <E: PredicateExpression, T: RawRepresentable> (lhs: E, rhs: T) -> Predicate<E.Root> where T.RawValue: Equatable & PredicateExpressionPrimitive {
+    .comparison(.init(lhs, .equal, rhs.rawValue))
 }
 
 @_disfavoredOverload
